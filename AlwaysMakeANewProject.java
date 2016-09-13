2543327c9e082d39a8482583f4f958e10f2948fa
ZRoberts2625 <zroberts@utcmediacityuk.org.uk>
2016-09-13 10:42:28 +0100

    Add files via upload

---
+++ AlwaysMakeANewProject.java
@@ -0,0 +1,54 @@
+
+package alwaysmakeanewproject;
+public class AlwaysMakeANewProject {
+    public static void main(String[] args) {
+        String Colour = "Orange";
+        String Name = "Zack";
+        String Street = "Roving Bridge Rise";
+        
+            System.out.println(Colour);
+            System.out.println(Name);
+            System.out.println(Street);
+        
+        int number = 5;
+        int age = 16;
+        int shoeSize = 11;
+        
+            System.out.println("Your chosen number was: " + number);
+            System.out.println("Your age is: " + age);
+            System.out.println("Your shoe size is: " + shoeSize);
+        
+        double test = 1.5;
+        double pi = 3.14;
+        
+            System.out.println(test);
+            System.out.println(pi);
+            // Area of a circle = Pi*R^2
+            // Speed = Distance/Time
+            // Note: (*)Multiplication (/)Division (+)Addition (-)Subtraction
+            System.out.println(age + shoeSize);
+            System.out.println(age - shoeSize);
+            System.out.println(age * shoeSize);
+            System.out.println(age / shoeSize);
+            
+        int x = 10;
+        int y = 5;
+            
+        if( x < y) {
+        //If condition is true
+            System.out.println("X is less than Y");
+        } else {
+        //If condition is false
+            System.out.println("X is more than Y");    
+         
+                 }
+        echo "# Lessons" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ZRoberts2625/Lessons.git
+git push -u origin master
+            
+    }
+    
+}
